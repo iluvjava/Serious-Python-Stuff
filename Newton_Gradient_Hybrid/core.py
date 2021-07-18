5ea0d028a99f71c7c35f3caaeb365d185fe52349
alto <victor1301166040@gmail.com>
2021-07-17 23:45:08 -0700

    a lot

--- Newton_Gradient_Hybrid/core.py
+++ Newton_Gradient_Hybrid/core.py
@@ -1,7 +1,12 @@
+"""
+Blow are 2 optimizer that can overcome inflection points, because of the mixed use of Newton's method and
+accelerated gradient method.
+
+
+"""
+
 import numpy as np
 import math as math
-
-
 
 def HybridNewtonGradient_Scalar(
         f: callable,
@@ -9,24 +14,30 @@ def HybridNewtonGradient_Scalar(
         ddf:callable,
         x0:float,
         eta:float,
+        momentum:float = 0.2,
         maxitr=10
     ):
     Xpre = x0
+    velocity = 0
     G = df(Xpre)
     for I in range(maxitr):
-        XGradient = Xpre - eta*G
+        velocity = momentum*velocity - eta*df(Xpre + momentum*velocity)
+        XGradient = Xpre + velocity
         # Try newton if gradient is accelerating, else don't try newton.
         Gnex = df(XGradient)
         if abs(Gnex) < abs(G):
-            Xnewton = Xpre - G / ddf(Xpre)
-            if f(XGradient) < f(Xnewton):
-                Xnext = XGradient
-                Which = "Tried Gradient and Newton, Gradient is better"
-                G = Gnex
-            else:
+            ddfXpre = ddf(Xpre)
+            Xnewton = Xpre - G/ddfXpre
+            if f(Xnewton) < f(XGradient) and abs(ddfXpre) > 1e-8:
                 Xnext = Xnewton
                 Which = "Tried Gradient and Newton, Newton is better"
                 G = df(Xnext)
+                velocity = 0
+            else:
+                Xnext = XGradient
+                Which = "Tried Gradient and Newton, Gradient is better"
+                G = Gnex
+
         else:
             Which = "Gradient, did't try Newton"
             Xnext = XGradient
@@ -40,6 +51,7 @@ def SecantGradient_Scalar(
         df:callable,
         x0:float,
         eta:float=0.1,
+        momentum:float=0.2,
         maxitr:int=100
     ):
     def finiteDiff(y1, y2, dx):
@@ -49,14 +61,19 @@ def SecantGradient_Scalar(
     Gpre = df(Xpre)
     Xnext = Xpre - eta*Gpre
     Gnext = df(Xnext)
+    velocity = Xnext - Xpre
     yield Xnext
-
     for _ in range(maxitr):
-        Xnext2 = Xnext - eta*Gnext
-        if abs(Gnext) < abs(Gpre) and abs(Xnext2 - Xpre) > 1e-8:
-            Xnewton = Xnext - Gnext/finiteDiff(Gpre, Gnext, Xnext - Xpre)
+        velocity = momentum*velocity - eta*df(Xpre + momentum*velocity)
+        Xnext2 = Xnext + velocity
+        if Xnext2 - Xnext == 0:
+            return Xnext2
+        H = finiteDiff(Gpre, Gnext, Xnext - Xpre)
+        if abs(Gnext) < abs(Gpre) and H > 1e-8:
+            Xnewton = Xnext - Gnext/H
             if f(Xnewton) < f(Xnext2):
                 Xnext2 = Xnewton
+                velocity = 0
             yield Xnext2
         else:
             yield Xnext2
@@ -65,14 +82,15 @@ def SecantGradient_Scalar(
 
 
 def main():
-    a = 1 + 1e-4
+    a = 1
     f = lambda x: (math.sin(x) + a*x)**2
     df = lambda x: 2*(math.sin(x) + a*x)*(math.cos(x) + a)
     ddf = lambda x: 2*(math.sin(x) + a*x)*(-math.sin(x)) + 2*(math.cos(x) + a)**2
-    x0 = 20000.3*math.pi
-    eta = 0.1
-    for I, (X, W) in enumerate(HybridNewtonGradient_Scalar(
-        f, df, ddf, x0, eta, maxitr=1000
+    x0 = 10.2*math.pi
+    eta = 0.2
+    momentum = 0.3
+    for I, (X, W) in enumerate(HybridNewtonGradient_Scalar (
+        f, df, ddf, x0, eta, momentum=momentum, maxitr=1000
     )):
         print(f"X: {X}, Strategy Chose: {W}")
         if f(X) < 1e-14:
@@ -80,14 +98,12 @@ def main():
     print(f"Gradient Newton's Hybrid Iterations Count: {I}, Objval: {f(X)}")
 
     for I, X in enumerate(SecantGradient_Scalar(
-        f, df, x0, eta, maxitr=1000
+        f, df, x0, eta, maxitr=100, momentum=momentum
     )):
         print(f"X: {X}")
         if f(X) < 1e-14:
             break
     print(f"Secant Gradient Hybrid Iterations Count: {I}, Objval: {f(X)}")
-
-
 
 
 if __name__ == "__main__":
--- Newton_Gradient_Hybrid/core2.py
+++ Newton_Gradient_Hybrid/core2.py
@@ -5,13 +5,108 @@
     When gradient is small, it start to random sample using local hessian and the sequence of descned parameters.
 """
 
-class MyHybridOptimizer:
-    def __int__(this):
+import numpy as np
+
+class HybridOptimizer:
+    """
+        A class that binds together:
+         * a Newton's method using hessian approximated by the gradient
+         * A nesterov accelerated gradient method.
+
+    """
+    def __init__(
+            _,
+            f:callable,     # objective function
+            df:callable,    # derivative of the function
+            x0:np.ndarray,  # initial guess for optimization
+            eta:float,      # the learning rate.
+            momentum:float):
+        """
+
+        :param f:
+        :param df:
+        :param x0:
+        :param eta:
+        :param momentum:
+        :return:
+        """
+        _.f, _.df = f, df
+        _._Momentum = momentum
+        _._Eta = eta
+
+        # running parameters for optimization:
+        _._Xpre = x0
+        _._Xnex = None
+        _._Gpre = df(x0)
+        _._Gnex = None
+        _._Velocity = 0
+        _._H = None # Hessian using 2 gradients.
+
+        # Running parameters for benchmarking and analyzing.
+        _.Xs = [_._Xpre]
+        _.Report = ""
+        _._Initialize()
+
+    def _Initialize(_):
+        _._Velocity = _._Momentum * _._Velocity - _.df(_._Xpre)
+        _._Xnex = _._Xpre + _._Velocity
+        _._Gnex = _.df(_._Xnex)
+        _._H = (_._Gnex - _._Gpre.T) / (_._Xnex - _._Xpre.T)
+        _.Xs.append(_._Xnex)
+
+    def _UpdateRunningParams(_, Xnex):
+        _._Xpre, _._Xnex = _._Xnex, Xnex
+        _.Xs.append(Xnex)
+        _._G, _._Gnex = _._Gnex, _.df(Xnex)
+        _._H = (_._Gnex - _._Gpre.T)/(_._Xnex - _._Xpre.T)
+
+    def _TrySecant(_):
+        df1, df2 = _._Gpre, _._Gnex
+        assert df2.ndim == 2, "expected gradient to be 2d np array. "
+        pinv = np.linalg.pinv
+        H = _._H
+        Xnex = _._Xpre - pinv(H)@df2
+        return Xnex
+
+    def _TryAccGradient(_):
+        x1, x2 = _._Xpre, _._Xnex
+        eta = _._Eta
+        v, m = _._Velocity, _._Momentum
+        _._Velocity = m * v - eta*_.df(x2 + v)  # Update velocity!
+        Xnex = x2 + _._Velocity
+        return Xnex
+
+    def __call__(_):
+        """
+            Call on this function to get the next point that it will step into.
+        :return:
+            The point that is chosen next
+        """
+        df1, df2 = _._Gpre, _._Gnex
+        f = _.f
+        norm = np.linalg.norm
+        Xnex = _._TryAccGradient()
+        if norm(df2) < norm(df1) and norm(df2) > 1e-8 and norm(_._H) > 1e-8:   # Try newton
+            XnexNewton = _._TrySecant()
+            _._Velocity = 0         # reset velocity.
+            if norm(f(XnexNewton)) < norm(f(Xnex)):
+                Xnex = XnexNewton
+        _._UpdateRunningParams(Xnex)
+        return Xnex
+
+    def Generate(_):
+
         pass
 
 
+
+
 def main():
-    pass
+    f, df = lambda x: x**2, lambda x: 2*x
+    optim = HybridOptimizer(f, df, np.array([[10], [10]]), 0.001, 0.2)
+    for I in range(3):
+        print(optim())
+
 
 
 if __name__ == "__main__":
